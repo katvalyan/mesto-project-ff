7d98a352cdeb4288987823e026c275842750db81
katvalyan Asya <asya.katvalyan.02@bk.ru>
2025-03-28 08:43:13 +0300

    Add create card and it is deleate

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
-// @todo: Темплейт карточки
+// Функция создания карточки
+function aboutCard(cardData, deleteCallback) {
+  // // Темплейт карточки
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  // // DOM узлы
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+  // // Функция Callback
+  deleteButton.addEventListener("click", function () {
+    deleteCallback(cardElement);
+  });
 
-// @todo: DOM узлы
+  return cardElement;
+}
 
-// @todo: Функция создания карточки
+// // Функция удаления карточки
+function DeleteCard(cardElement) {
+  cardElement.remove();
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+// // Вывод карточки на страницу
+const placesList = document.querySelector(".places__list");
+initialCards.forEach((cardData) => {
+  const cardElement = aboutCard(cardData, DeleteCard);
+  placesList.appendChild(cardElement);
+});
